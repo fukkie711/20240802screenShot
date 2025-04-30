699c200cfea0839a02e81a47ad7914072ee614de
fukkie711 <k-fukuzawa@aitech.ac.jp>
2025-04-30 12:41:25 +0900

    fixed:resolution problem

--- kindle-screenshot.py
+++ kindle-screenshot.py
@@ -4,29 +4,34 @@ from PIL import Image
 import os
 from datetime import datetime
 
+screen = pyautogui.size()
+print(f'Screen size:{screen}')
+
+# 待機（この間に画面を手動で変える？）
+time.sleep(2)
+
 def capture_and_click(num_loops):
     screenshots = []
     for i in range(num_loops):
 
+        print(f'Page:{i}')
+        # ※全画面を撮ってからクロップすると解像度が落ちない．
+        # 全画面を撮る（Macのディスプレイ）
+        screenshot = pyautogui.screenshot()
+        # print(pyautogui.locateOnScreen(screenshot))
 
-        # 座標(Mac)
-        # left_up=[115,56]
-        # left_down=[0,900]
-        # right_up=[1440,56]
-        # right_down=[1440,900]
+        # クロップする
+        # 新書1ページ
+        croped = screenshot.crop((820, 0, 2060, 1800))
 
-        # スクリーンショットを撮影
-        screenshot = pyautogui.screenshot(region=[115,56,1330,900])
-        screenshots.append(screenshot)
+        screenshots.append(croped)
   
-        # pyautogui.click(x=32, y=475, button="left", clicks=num_loops, interval=0.5)
-        pyautogui.click(x=32, y=475, button="left")
+        # ページ送り（右の「次へ」ボタン）
+        pyautogui.click(x=1400, y=450, button="left", interval=0.5)
 
-        # 待機
-        time.sleep(0.5)
-    
     #ファイル名のための日時取得
     now = datetime.now().strftime('%Y%m%d%H%M%S')
+    print(f'保存中...{now}')
     # スクリーンショットをPDFとして保存
     if screenshots:
         screenshots[0].save(now + "_screenshots.pdf", save_all=True, append_images=screenshots[1:])
@@ -34,4 +39,4 @@ def capture_and_click(num_loops):
     print(f"{num_loops}回のスクリーンショットとクリックが完了しました。PDFが保存されました。")
 
 # 使用例
-capture_and_click(num_loops=99)
+capture_and_click(num_loops=330)
